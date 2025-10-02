225a1adabfaf02e84bdacd8660d3a2f8e140f8ca
Max Rakitin <mrakitin@bnl.gov>
2025-10-02 18:58:03 -0400

    TST: add import tests for `numba-cuda`

--- import-tests.py
+++ import-tests.py
@@ -10,6 +10,7 @@ import nslsii
 import numexpr
 import larch
 import larch.xrd
+from numba import cuda
 
 parser = argparse.ArgumentParser(description="Test import of various Python packages.")
 parser.add_argument(
@@ -35,3 +36,4 @@ print(f"{tiled.__version__ = }")
 print(f"{nslsii.__version__ = }")
 print(f"{numexpr.__version__ = }")
 print(f"{larch.__version__ = }")
+print(cuda.__file__)
